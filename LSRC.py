264f72fb4ae139933d3450572c531704a0d8366a
Renan Lucas <renanluscad@gmail.com>
2024-08-01 16:11:19 -0300

    Update LSRC

--- LSRC.py
+++ LSRC.py
@@ -1,1 +1,15 @@
+# Progressbar
+from tqdm import tqdm
+import time
+import importlib 
+libraries = [
+    'os',
+    'time',
+
+]
+for lib in tqdm(libraries, desc="Loading libraries"):
+    importlib.import_module(lib)
+    time.sleep(1.2)
+
 # Experimental Ports Scripty
+print("This is an experimental ports")
