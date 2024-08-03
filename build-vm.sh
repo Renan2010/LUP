4afad3b3da0b5c48cdcbe05911bafb09085dce23
Renan Lucas <renanluscad@gmail.com>
2024-08-03 14:17:08 -0300

    Update build-vm.sh

--- build-vm.sh
+++ build-vm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "==> Creating an VM for LSRC please wait"
+echo "==> Creating an VM for LUP please wait"
 python3 -m venv LUP-vm
 echo "==> Instaling dependencies"
 source LUP-vm/bin/activate
 pip3 install npyscreen
 pip3 install tqdm
-echo "==> LSRC-vm was successfully created"
+echo "==> LUP-vm was successfully created"
