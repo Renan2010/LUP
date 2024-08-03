91a716a4e0d244ed75f3e612a816db1aa09d5351
Renan Lucas <renanluscad@gmail.com>
2024-08-03 14:27:29 -0300

    Add files via upload

--- Start.sh
+++ Start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo "Starting LSRC"
+echo "Starting LUP"
 source LUP-vm/bin/activate
 python3 LUP.py
+source /etc/profile
--- build-vm.sh
+++ build-vm.sh
@@ -5,4 +5,8 @@ echo "==> Instaling dependencies"
 source LUP-vm/bin/activate
 pip3 install npyscreen
 pip3 install tqdm
+clear
+echo "==> Refresh Profile"
+source /etc/profile
 echo "==> LUP-vm was successfully created"
+exit
