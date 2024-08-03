fad49543f452e1e8ed5f07da7868d1b7699c62fd
Renan Lucas <renanluscad@gmail.com>
2024-08-03 14:03:48 -0300

    Fix error build-vm.sh

--- build-vm.sh
+++ build-vm.sh
@@ -2,7 +2,7 @@
 echo "==> Creating an VM for LSRC please wait"
 python3 -m venv LUP-vm
 echo "==> Instaling dependencies"
-source LSRC-vm/bin/activate
+source LUP-vm/bin/activate
 pip3 install npyscreen
 pip3 install tqdm
 echo "==> LSRC-vm was successfully created"
